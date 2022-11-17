64adf9ca1c293413e08926358608f7b514707ee8
MircioagaIulia <105177021+MircioagaIulia@users.noreply.github.com>
2022-11-17 23:26:56 +0200

    4. Update c++ file (added set_pret methods).

--- src/tema1.cpp
+++ src/tema1.cpp
@@ -9,34 +9,49 @@ class Laptop
         char *processor;
         int pret;
   public:
-        Laptop(char* device_name, int id_laptop, int RAM, char* processor){
+        Laptop(char* device_name, int id_laptop, int RAM, char* processor);
+        ~Laptop();
+
+        Laptop()=delete;
+
+
+    void set_pret(int pret);
+    void set_pret(float pret);
+    void set_pret(int pret, int reducere);
+ 
+ 
+    void print();
+};
+
+Laptop:: Laptop(char* device_name, int id_laptop, int RAM, char* processor){
           this->device_name=device_name;
           this->id_laptop=id_laptop;
           this->RAM=RAM;
           this->processor=processor;
           std::cout<<"Constructor Laptop "<<std::endl;
         }
-        ~Laptop(){
+Laptop:: ~Laptop(){
            std::cout<<"Apel destructor Laptop "<<std::endl;
-        }
-
-
-         Laptop()=delete;
-
-
-    void set_pret(int pret){
+}
+   
+void Laptop:: set_pret(int pret){
        this->pret=pret;
        std::cout<<"Laptopul "<<this->device_name<<" costa "<<pret<<" lei"<<"\n\n";
-       
+}    
 
-    }    
- 
-  void print(){
+void Laptop:: set_pret(float pret){
+       this->pret=pret;
+       std::cout<<"Laptopul "<<this->device_name<<" costa "<<pret<<" lei"<<"\n\n";
+}    
+
+void Laptop:: set_pret(int pret, int reducere){
+       this->pret=pret-reducere;
+       std::cout<<"Laptopul "<<this->device_name<<" costa "<<this->pret<<" lei"<<"\n\n";
+}  
+
+void Laptop:: print(){
     std::cout<<"Device name:  "<<this->device_name<<" \n"<<"Id laptop: "<<this->id_laptop<<" \n"<<"RAM: "<<this->RAM<<" GB"<<" \n"<<"Processor: "<<this->processor<<" \n\n";
- }
-  
-
-};
+}
 
 class Laptop_gaming:public Laptop{
 protected: 
@@ -45,68 +60,72 @@ protected:
 
   
   public:
-  Laptop_gaming(char *device_name, int id_laptop, int RAM, char *processor, int refresh_rate, char* GPU):Laptop(device_name, id_laptop, RAM, processor){
+  Laptop_gaming(char *device_name, int id_laptop, int RAM, char *processor, int refresh_rate, char* GPU);
+
+  ~Laptop_gaming();
+
+   Laptop_gaming()=delete;
+
+   void print();
+   
+
+};
+
+Laptop_gaming:: Laptop_gaming(char *device_name, int id_laptop, int RAM, char *processor, int refresh_rate, char* GPU):Laptop(device_name, id_laptop, RAM, processor){
     
     this->refresh_rate=refresh_rate;
     this->GPU=GPU;
     std::cout<<"Constructor Laptop gaming \n";
  }
 
-   Laptop_gaming()=delete;
+Laptop_gaming:: ~Laptop_gaming(){
+     std::cout<<"Apel destructor Laptop_gaming "<<std::endl;
+  };
 
-  void print(){
+
+void Laptop_gaming:: print(){
     std::cout<<"Device name:  "<<this->device_name<<" \n"<<"Id laptop: "<<this->id_laptop<<" \n"<<"RAM: "<<this->RAM<<" GB"<<" \n"<<"Processor: "<<this->processor<<" \n";
     std::cout<<"Refresh_rate "<<this->refresh_rate<<" Hz \n"<<"GPU "<<this->GPU<<" \n\n";
 
- }
-
-  ~Laptop_gaming(){
-     std::cout<<"Apel destructor Laptop_gaming "<<std::endl;
-  };
-   
-
-};
+}
 
 class Laptop_2_in_1: public Laptop{
     protected:
     bool touchscreen;
     public:
-    Laptop_2_in_1(char *device_name, int id_laptop, int RAM, char *processor, bool touchscreen):Laptop(device_name, id_laptop, RAM, processor){
-    
-    this->touchscreen=touchscreen;
-    std::cout<<"Constructor Laptop 2 in 1\n";
- }
- ~Laptop_2_in_1(){
-     std::cout<<"Apel destructor Laptop_2_in_1"<<std::endl;
-  };
+    Laptop_2_in_1(char *device_name, int id_laptop, int RAM, char *processor, bool touchscreen);
+
+   ~Laptop_2_in_1();
    Laptop_2_in_1()=delete;
 
  
-  void print(){
+    void print();
+ };
+
+Laptop_2_in_1:: Laptop_2_in_1(char *device_name, int id_laptop, int RAM, char *processor, bool touchscreen):Laptop(device_name, id_laptop, RAM, processor){
+   
+    this->touchscreen=touchscreen;
+    std::cout<<"Constructor Laptop 2 in 1\n";
+ }
+Laptop_2_in_1:: ~Laptop_2_in_1(){
+     std::cout<<"Apel destructor Laptop_2_in_1"<<std::endl;
+}
+   
+
+ 
+void Laptop_2_in_1:: print(){
      std::cout<<"Device name:  "<<this->device_name<<" \n"<<"Id laptop: "<<this->id_laptop<<" \n"<<"RAM: "<<this->RAM<<" GB"<<" \n"<<"Processor: "<<this->processor<<" \n";
      std::cout<<"Touchscreen "<<this->touchscreen<<" \n";
  
  }
- };
- class Laptop_copie{
-   public:
-   int nr;
-   public:
-   Laptop_copie(int nr){
-      this->nr=nr;
-   };
-   ~Laptop_copie(){
-      std::cout<<"destructor";
-   }
-   void afisare(){
-      std::cout<<"ana are mere ";
- }}
- ;
+
+
 
 int main()
 { Laptop* l=new Laptop("TLMP208W", 125, 8, "Intel I7");
    l->print();
    l->set_pret(1200);
+   l->set_pret(1200, 90);
 
    Laptop_gaming* lg=new Laptop_gaming("TMPL600W", 127, 16, "Intel I5", 60, "nVidia");
    lg->print();
